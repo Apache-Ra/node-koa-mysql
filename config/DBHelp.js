40e43066700761a1ad1135fe65a6f70042a8a491
Apache-Ra <summer.vip.20@qq.com>
2019-04-09 11:29:10 +0800

    ······

---
+++ config/DBHelp.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('test', 'root', 'summer', {
+    host: 'localhost',
+    dialect: 'mysql',
+    operatorsAliases: false,
+    dialectOptions: {
+        // 字符集
+        charset: "utf8mb4",
+        collate: "utf8mb4_unicode_ci",
+        supportBigNumbers: true,
+        bigNumberStrings: true
+    },
+
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+    //东八时区
+    timezone: '+08:00'
+});
+
+module.exports = {
+    sequelize
+}
